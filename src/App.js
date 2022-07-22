7776855cc8fbb7063783d16786c0071a92165f1f
Misha Melnik <misha.melnik2016@gmail.com>
2022-07-22 11:31:28 +0300

    created layout in experience component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 // COMPONENTS
 import Banner from './components/Banner/Banner';
 import Header from './components/Header/Header';
+import Experience from './components/Experience/Experience';
 
 const App = () => {
   return (
     <div className="relative overflow-hidden before:w-[600px] before:bg-circle before:h-[200px] before:bg-no-repeat before:absolute before:-top-16 before:left-[600px] before:hidden before:lg:flex">
       <Header />
       <Banner />
+      <Experience />
     </div>
   );
 };
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+// IMG
+import img1 from '../../assets/img/exp-img1.png';
+import img2 from '../../assets/img/exp-img2.png';
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <div className="mb-12 lg:mb-24">
+      <div className="container mx-auto">
+        <div className="flex flex-col min-h-[480px] lg:space-x-20 lg:flex-row">
+          <div className="flex-1 flex space-x-6 items-center lg:space-x-12">
+            <div className="self-start">
+              <img src={img1} alt="exampleOne" />
+            </div>
+            <div className="self-end">
+              <img src={img2} alt="exampleTwo" />
+            </div>
+            <div className="flex-1 flex flex-col items-start justify-center mt-6 lg:mt-0">
+              <h2 className="text-3xl font-bold mb-6">New Experience In Playing Game</h2>
+              <p className="font-secondary mb-6">
+                You can try playing the game with a new style and of course a more real feel, like you are the main
+                character in your game and adventure in this new digital world.
+              </p>
+              <button className="btn">Get it now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
